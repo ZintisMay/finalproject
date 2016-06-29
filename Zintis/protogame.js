8277f652efe7076082c6b2bc8f307d36236279bd
ZintisMay <zbmayk@gmail.com>
2016-06-29 16:30:28 -0400

    working on begin() in protogame

--- Zintis/protogame.js
+++ Zintis/protogame.js
@@ -7,6 +7,10 @@ var hugeString = "";
 var words = [];
 var time = [];
 var counter = 0;
+var points = 0;
+var loseGame = 0;
+
+
 
 //start the game - makes some words and calls up the starting functions.
 fs.readFile('./string.txt', "utf-8", (err, data) => {
@@ -21,8 +25,8 @@ fs.readFile('./string.txt', "utf-8", (err, data) => {
 	console.log(wordsArray.length);
 	console.log(words);
 	console.log(time);
-	wordEntry();
-	iterate();
+
+	beginGame();
 });
 
 // takes a string of words and spaces and turns it into an array
@@ -47,18 +51,51 @@ function stringTextBreaker(x){
 }
 
 function beginGame(){
+	logger();
+	console.log("Welcome to NodeType");
+	logger();
+	console.log("Choose from the following menu:")
+	console.log("1: Play!");
+	console.log("2: See High Scores!");
+	console.log("3: Exit");
+	
+	prompt.get(['userInput'],function(err, result){
+		
+
+		if (result.userInput == 1){
+			
+			wordEntry();
+			var iterate = setInterval(function(){ 
+				countDown();
+				newWord();
+				gameStateLog();	
+			}, 1000);
+		
+		}else if (result.userInput == 2){
+			
+			logger();		
+			for (x=0;x<highScores.length;x++){
+
+				console.log(highScores[x].name, ": ", highScores[x].score);
+			}
+			beginGame();
+		
+		}else if (result.userInput == 3){
+			logger();
+			console.log("thanks for playing!");
+			logger();
+			
+			process.exit();
+		
+		}else {console.log("invalid input, try again.");beginGame();}
+
+	});
 
 }
 
 //this is the timer. every x seconds it counts town the word's timer, makes a new word, and logs the gamestate
-function iterate(){
-	setInterval(function(){ 
 
-		countDown();
-		newWord();
-		gameStateLog();	
-	}, 4000);
-}
+
 
 //randomly plucks word from wordlist, adds to the appropriate arrays
 function newWord(){
@@ -70,8 +107,13 @@ function newWord(){
 //prints out the gamestate for the player
 function gameStateLog(){
 	logger();
-	for (x=words.length-1;x>=0;x--){
-		console.log(words[x], time[x]);
+	for (x=5;x>=0;x--){
+		if (words[x]){
+			console.log(words[x], time[x]);
+		}else{
+			console.log("BLANK");
+		}
+
 	}
 	logger();
 }
@@ -92,13 +134,30 @@ function wordEntry(){
 // every x seconds it ticks down the timer array and deletes words that have 0 seconds on the timer
 function countDown(){
 
-	for(x=0;x<time.length;x++){
+	for(x=0;x<(time.length-1);x++){
 
 		time[x] = time[x]-1;
 
 		if (time[x] == 0){
 
+			loseGame += 1;
 			spliceTimeWord(x);
+
+			if (loseGame == 5) {
+
+				clearInterval(iterate);
+				
+				prompt.get(['userName'],function(err, result){
+
+					highScores.push({name: result.userName ,score: points});
+					resetGame();
+				});
+
+				logger();
+				console.log("YOU LOSE, YOU HAD ", point, " POINTS!");
+				logger();
+				beginGame();
+			}
 
 		}
 	}
@@ -109,6 +168,14 @@ function spliceTimeWord(x){
 	time.splice(x, 1);
 	words.splice(x, 1);
 
+}
+
+// resets all base values
+function resetGame(){
+	time = [];
+	words = [];
+	points = 0;
+	loseGame = 0;
 }
 
 //just makes the console easier to read
